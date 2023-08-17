d47f762171277e284de5c710efa1f1bb4a3343d3
evavanderkroft <evavanderkroft@gmail.com>
2023-08-17 13:13:00 +0200

    update on adding  and deleting favorites from list

--- FestivalAppie/src/Favorites.js
+++ FestivalAppie/src/Favorites.js
@@ -1,4 +1,140 @@
-import React, { useEffect } from "react";
+// import React, { useEffect, useState } from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   FlatList,
+//   SafeAreaView,
+//   StatusBar,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+
+// export default function Favorites(props) {
+//   const favoritesArray =
+//     props.route.params && props.route.params.favorites
+// ? props.route.params.favorites
+//       : [];
+//   const [favorites, setFavorites] = useState(favoritesArray);
+
+//   useEffect(() => {
+//     console.log(favorites);
+//   }, [favorites]);
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.title}>Favorite Markers</Text>
+//       {favorites.length > 0 ? (
+//         <FlatList
+//           data={favorites}
+//           renderItem={({ item }) => (
+//             <TouchableOpacity
+//               onPress={() =>
+//                 props.navigation.navigate("Map", { currentMarker: item })
+//               }
+//             >
+//               <View style={styles.item}>
+//                 <Text style={styles.itemText}>{item.title}</Text>
+//               </View>
+//             </TouchableOpacity>
+//           )}
+//         />
+//       ) : (
+//         <Text style={styles.title}>No favs</Text>
+//       )}
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//     fontFamily: "Gill Sans",
+//   },
+//   item: {
+//     backgroundColor: "#9147FF",
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//     fontFamily: "Gill Sans",
+//   },
+//   bodyText: {
+//     fontSize: 14,
+//     color: "#29292E",
+//     fontFamily: "Gill Sans",
+//   },
+// });
+
+// import React, { useEffect, useState } from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   FlatList,
+//   SafeAreaView,
+//   StatusBar,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+
+// export default function Favorites(props) {
+//   const favoritesArray = props.route.params?.favorites || [];
+//   const [favorites, setFavorites] = useState(favoritesArray);
+
+//   useEffect(() => {
+//     console.log(favorites);
+//   }, [favorites]);
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.title}>Favorite Markers</Text>
+//       {favorites.length > 0 ? (
+//         <FlatList
+//           data={favorites}
+//           renderItem={({ item }) => (
+//             <TouchableOpacity
+//               onPress={() =>
+//                 props.navigation.navigate("Map", { currentMarker: item })
+//               }
+//             >
+//               <View style={styles.item}>
+//                 <Text style={styles.itemText}>{item.title}</Text>
+//               </View>
+//             </TouchableOpacity>
+//           )}
+//         />
+//       ) : (
+//         <Text style={styles.title}>No favs</Text>
+//       )}
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//     fontFamily: "Gill Sans",
+//   },
+//   item: {
+//     backgroundColor: "#9147FF",
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//     fontFamily: "Gill Sans",
+//   },
+//   bodyText: {
+//     fontSize: 14,
+//     color: "#29292E",
+//     fontFamily: "Gill Sans",
+//   },
+// });
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,29 +142,59 @@ import {
   SafeAreaView,
   StatusBar,
   TouchableOpacity,
-  Button,
+  View,
 } from "react-native";
 
 export default function Favorites(props) {
+  const favoritesArray = props.route.params?.favorites || [];
+  const [favorites, setFavorites] = useState(favoritesArray);
+  const [hasFavorites, setHasFavorites] = useState(favorites.length > 0);
+
   useEffect(() => {
-    console.log(props.route.params.favorites);
-  }, []);
+    const focusListener = props.navigation.addListener("focus", () => {
+      // Update favorites when the screen receives focus
+      if (props.route.params && props.route.params.favorites) {
+        setFavorites(props.route.params.favorites);
+        setHasFavorites(props.route.params.favorites.length > 0);
+      } else {
+        console.log("empty");
+      }
+    });
+
+    return () => {
+      focusListener(); // Clean up the focus listener when the component unmounts
+    };
+  }, [props.navigation, props.route.params]);
+
+  // const handleRemoveFavorite = (item) => {
+  //   const updatedFavorites = favorites.filter(
+  //     (fav) => fav.title !== item.title
+  //   );
+  //   setFavorites(updatedFavorites);
+  //   setHasFavorites(updatedFavorites.length > 0);
+  // };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Favorite Markers</Text>
-      <FlatList
-        data={props.route.params.favorites}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() =>
-              props.navigation.navigate("Map", { currentMarker: item })
-            }
-          >
-            <Text style={styles.item}>{item.title}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {hasFavorites ? (
+        <FlatList
+          data={favorites}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() =>
+                props.navigation.navigate("Map", { currentMarker: item })
+              }
+            >
+              <View style={styles.item}>
+                <Text style={styles.itemText}>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      ) : (
+        <Text style={styles.NoFavsText}>No favs</Text>
+      )}
     </SafeAreaView>
   );
 }
@@ -37,6 +203,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    margin: 20,
+    // alignContent: "center",
+    // alignItems: "center",
     fontFamily: "Gill Sans",
   },
   item: {
@@ -44,14 +213,26 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 32,
     fontFamily: "Gill Sans",
+    alignSelf: "center",
+    margin: 10,
   },
-  bodyText: {
-    fontSize: 14,
-    color: "#29292E",
+  itemText: {
+    fontSize: 16,
+    color: "#FFFFFF",
+    fontFamily: "Gill Sans",
+  },
+  removeButton: {
+    color: "red",
+  },
+
+  NoFavsText: {
+    fontSize: 24,
     fontFamily: "Gill Sans",
   },
 });
--- FestivalAppie/src/List.js
+++ FestivalAppie/src/List.js
@@ -1,3 +1,98 @@
+// import React, { useState, useEffect } from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   FlatList,
+//   SafeAreaView,
+//   StatusBar,
+//   TouchableOpacity,
+//   Button,
+// } from "react-native";
+
+// export default function List({ markers, navigation }) {
+//   //rendering the flatlist. On press, go to map with that marker.
+//   const [favorites, setFavorites] = useState([]);
+
+//   useEffect(() => {
+//     if (favorites.length > 0) {
+//       navigation.navigate("Favorites", { favorites });
+//     }
+//     // if (favorites.length === 0) {
+//     //   favorites = [];
+//     // }
+
+//     // if (favorites.length === 0) {
+//     //   console.log("empty favs");
+//     //   favorites = [];
+//     // }
+//   }, [favorites]);
+
+//   const toggleFavorite = (item) => {
+//     if (favorites.some((fav) => fav.title === item.title)) {
+//       setFavorites((currentFavorites) =>
+//         currentFavorites.filter((fav) => fav.title !== item.title)
+//       );
+//     } else {
+//       setFavorites((currentFavorites) => [...currentFavorites, item]);
+//     }
+//   };
+
+//   const isFavorite = (item) =>
+//     favorites.some((fav) => fav.title === item.title);
+
+//   const renderItem = ({ item }) => (
+//     <TouchableOpacity
+//       onPress={() => navigation.navigate("Map", { currentMarker: item })}
+//       style={styles.item}
+//     >
+//       <Text style={styles.title}>{item.title}</Text>
+//       <Text style={styles.bodyText}>{item.description}</Text>
+//       <Button
+//         style={styles.buttonFavorites}
+//         title={isFavorite(item) ? "Remove from Favorites" : "Add to Favorites"}
+//         color={isFavorite(item) ? "red" : "green"}
+//         onPress={() => toggleFavorite(item)}
+//       />
+//     </TouchableOpacity>
+//   );
+
+//   useEffect(() => {
+//     navigation.setParams({ favorites }); // Update favorites in navigation params
+//   }, [favorites]);
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList data={markers} renderItem={renderItem} />
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//     fontFamily: "Gill Sans",
+//   },
+//   item: {
+//     backgroundColor: "#9147FF",
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//     color: "#FFFFFF",
+//     fontFamily: "Gill Sans",
+//   },
+//   bodyText: {
+//     fontSize: 14,
+//     color: "#29292E",
+//     fontFamily: "Gill Sans",
+//   },
+//   buttonFavorites: {
+//     backgroundColor: "#FFFFFF",
+//   },
+// });
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
@@ -10,14 +105,29 @@ import {
 } from "react-native";
 
 export default function List({ markers, navigation }) {
-  //rendering the flatlist. On press, go to map with that marker.
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     if (favorites.length > 0) {
       navigation.navigate("Favorites", { favorites });
     }
-  }, [favorites]);
+  });
+
+  const toggleFavorite = (item) => {
+    if (favorites.some((fav) => fav.title === item.title)) {
+      const updatedFavorites = favorites.filter(
+        (fav) => fav.title !== item.title
+      );
+      console.log("Removing from favorites:", item.title);
+      setFavorites(updatedFavorites.length > 0 ? updatedFavorites : []);
+    } else {
+      console.log("Adding to favorites:", item.title);
+      setFavorites((currentFavorites) => [...currentFavorites, item]);
+    }
+  };
+
+  const isFavorite = (item) =>
+    favorites.some((fav) => fav.title === item.title);
 
   const renderItem = ({ item }) => (
     <TouchableOpacity
@@ -28,16 +138,16 @@ export default function List({ markers, navigation }) {
       <Text style={styles.bodyText}>{item.description}</Text>
       <Button
         style={styles.buttonFavorites}
-        title={
-          favorites.includes(item)
-            ? "Remove from Favorites"
-            : "Add to Favorites"
-        }
-        color={favorites.includes(item) ? "red" : "green"}
-        onPress={() => setFavorites((current) => [...current, item])}
+        title={isFavorite(item) ? "Remove from Favorites" : "Add to Favorites"}
+        color={isFavorite(item) ? "red" : "green"}
+        onPress={() => toggleFavorite(item)}
       />
     </TouchableOpacity>
   );
+
+  useEffect(() => {
+    navigation.setParams({ favorites }); // Update favorites in navigation params
+  }, [favorites]);
 
   return (
     <SafeAreaView style={styles.container}>
